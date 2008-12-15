d40eb153bfce2cade901d07332e12fbbf8bb8e76
Nathan Weizenbaum <nex342@gmail.com>
2008-12-14 22:16:34 -0800

    Add mtime checking.

--- mdb/__init__.py
+++ mdb/__init__.py
@@ -1,15 +1,26 @@
+import os
+import urllib
 from datetime import datetime
 
-from mdb.formats import MusicFile
 from couchdb.client import Server
 
+import mdb.util
+from mdb.formats import MusicFile
+
 SAVED_METATAGS = [
-    "~filename", "~format", "~mountpoint", "~performers", "~people",
-    "~#added", "~#bitrate", "~#length", "~#year", "~#mtime",
+    "~filename", "~dirname", "~format", "~mountpoint", "~performers",
+    "~people", "~#added", "~#bitrate", "~#length", "~#year", "~#mtime",
     "~#disc", "~#discs", "~#track", "~#tracks"
     ]
 
+SINGLETON_TAGS = ["~filename", "~dirname", "~format", "~mountpoint"]
+
 DEFAULTS = {"~#disc": 1, "~#discs": 1}
+
+def _id(path):
+    if isinstance(path, unicode):
+        path = path.encode("utf-8")
+    return urllib.quote(path, '')
 
 class Database:
     def __init__(self, server, name):
@@ -18,17 +29,29 @@ class Database:
         else: self.db = self.server.create(name)
 
     def add(self, path):
-        song = self.song_for(path)
+        song = self.song_for(os.path.realpath(path))
         if song is None: return
         self.db[song.key] = self.dict_for(song)
 
     def add_many(self, paths):
-        self.db.update(map(self.dict_for,
-                           filter(None,
-                                  map(self.song_for, paths))))
+        paths = map(os.path.realpath, paths)
+        view = self.db.view('_view/update/all')
+        def updated_file(path):
+            docs = list(view[_id(path)])
+            if not docs: return True
+            return util.mtime(path) > docs[0].value["mtime"]
+
+        songs = filter(None, map(self.song_for, filter(updated_file, paths)))
+        if not songs: return
+        songs = map(self.dict_for, songs)
+        for song in songs:
+            docs = list(view[song["_id"]])
+            if not docs: break
+            song["_rev"] = docs[0].value["_rev"]
+        self.db.update(songs)
 
     def song_for(self, path):
-        try: return MusicFile(path.decode("utf-8", "replace"))
+        try: return MusicFile(path)
         except IOError: return None
 
     def dict_for(self, song):
@@ -36,10 +59,13 @@ class Database:
         for tag in SAVED_METATAGS + song.realkeys():
             val = song(tag)
             if val:
-                if isinstance(val, basestring): val = val.split("\n")
+                if isinstance(val, basestring):
+                    val = util.fsdecode(val)
+                    if not tag in SINGLETON_TAGS:
+                        val = val.split("\n")
                 d[tag] = val
         for tag, default in DEFAULTS.items():
             if not tag in song: song[tag] = default
         # CouchDB doesn't like apostrophes in keys for some reason...
-        d["_id"] = song.key.replace("'", "_")
+        d["_id"] = _id(song.key)
         return d
